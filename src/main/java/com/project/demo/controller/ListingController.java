04417fcc0f8d947fe8827d4db475875c6ecc98ef
RipWatermelon <ripwatermelon@gmail.com>
2026-03-31 12:58:51 +0300

    more comments

--- src/main/java/com/project/demo/controller/ListingController.java
+++ src/main/java/com/project/demo/controller/ListingController.java
@@ -19,22 +19,22 @@ public class ListingController {
         return mapper.readValue(file, new TypeReference<List<Map<String, Object>>>() {}); //mapper converts json to java objects
     }
 
-    @PostMapping("/add-listing")
-    public List<Map<String, Object>> addListing(@RequestBody Map<String, Object> listing) throws IOException {
-        List<Map<String, Object>> listings = getAllListings();
-        int newId = listings.stream().mapToInt(l -> l.get("listingID") == null ? 0 : (int) l.get("listingID")).max().orElse(0) + 1;
-        listing.put("listingID", newId);
+    @PostMapping("/add-listing") //responds to add listing requests
+    public List<Map<String, Object>> addListing(@RequestBody Map<String, Object> listing) throws IOException { //takes json and turns it into a Map
+        List<Map<String, Object>> listings = getAllListings(); //loads everything that exists in, then adds
+        int newId = listings.stream().mapToInt(l -> l.get("listingID") == null ? 0 : (int) l.get("listingID")).max().orElse(0) + 1; // loop thru all the listings, grab each ID as an integer, find the highest id, (if theres no max then default to 0
+        listing.put("listingID", newId); // and then +1 to the highest ID
         listings.add(listing);
-        mapper.writeValue(new File(FILE_PATH), listings);
-        return listings;
+        mapper.writeValue(new File(FILE_PATH), listings); //write the entire updated list to the JSON file, and overwrite
+        return listings; //send the full thing back as API response
     }
 
-    @DeleteMapping("/remove-listing/{id}")
-    public List<Map<String, Object>> removeListing(@PathVariable int id) throws IOException {
+    @DeleteMapping("/remove-listing/{id}") //responds to that specific ID we input
+    public List<Map<String, Object>> removeListing(@PathVariable int id) throws IOException { // @PathVariable grabs the id from the URL and puts it into the id variable
         List<Map<String, Object>> listings = getAllListings();
-        listings.removeIf(l -> Objects.equals(l.get("listingID"), id));
-        mapper.writeValue(new File(FILE_PATH), listings);
-        return listings;
+        listings.removeIf(l -> Objects.equals(l.get("listingID"), id)); //removes listing if the ID matches the one we inputted
+        mapper.writeValue(new File(FILE_PATH), listings); //saves the smaller list back to the file
+        return listings; //return updated list as the response
     }
 }
 
